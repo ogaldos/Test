ca27497543db50af0ecee7d86aa4120d1a2c7286
OSCAR GALDOS LEORZA <oscar.galdos@daimler.com>
2021-04-09 13:25:31 +0200

    primeras pruebas

--- Hello.py
+++ Hello.py
@@ -1,4 +1,11 @@
-#%%
-print("Hello world")
-#%%
-print("Hola")
+print("Starting program...")
+
+import pygame
+import numpy as numpy
+#Donde coloquemos el simbolo #%% nos creará una linea
+#de comando donde podremos hacer Run Cell, Run Below, Debug Cell
+
+pygame.init()
+pygame.display.list_modes()
+
+print("The End")
